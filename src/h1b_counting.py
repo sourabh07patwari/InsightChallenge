80a0e4bb40e03f3cac14e8f2c860b9edde91d1d6
sourabh07 <sourabh07patwari@gmail.com>
2018-10-31 23:36:06 -0700

    Used csv library and added dictionaries to take state counts and occupation counts

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -2,13 +2,9 @@ import os.path as pth
 import csv
 
 
-def h1b_report_csv(filename):
+def parsecsv(filename, dict_states, dict_occupation):
 
     total_certified = 0
-    dict_states = {}
-    dict_occupation = {}
-    SAMPLES = list()
-
     with open(filename, 'r') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(), delimiters=';,')
         csvfile.seek(0)
@@ -28,7 +24,7 @@ def h1b_report_csv(filename):
                     if header == "CASE_STATUS":
                         case_status_index = header_index
 
-                    if header == "EMPLOYER_STATE":
+                    if header == "WORKSITE_STATE":
                         state_index = header_index
 
                     if header == "SOC_NAME":
@@ -36,11 +32,6 @@ def h1b_report_csv(filename):
                     header_index += 1
 
             else:
-                example = list()
-                example.append((row[case_status_index]))
-                example.append((row[occupation_index]))
-                example.append((row[state_index]))
-                SAMPLES.append(example)
 
                 if row[case_status_index] == "CERTIFIED":
                     total_certified += 1
@@ -49,43 +40,85 @@ def h1b_report_csv(filename):
                     if row[case_status_index] == "CERTIFIED":
                         dict_states[row[state_index]] += 1
                 else:
-                    dict_states[row[state_index]] = 1
-
-                if row[occupation_index] in dict_occupation:
                     if row[case_status_index] == "CERTIFIED":
-                        dict_occupation[row[occupation_index]] += 1
+                        dict_states[row[state_index]] = 1
+
+                string_occupation = row[occupation_index]
+                string_occupation = string_occupation.replace('"','')
+                if string_occupation in dict_occupation:
+                    if row[case_status_index] == "CERTIFIED":
+                        dict_occupation[string_occupation] += 1
                 else:
-                    dict_occupation[row[occupation_index]] = 1
+                    if row[case_status_index] == "CERTIFIED":
+                        dict_occupation[string_occupation] = 1
 
             num_rows += 1
-    print(SAMPLES)
-    print("Total Certified Professionals: " + str(total_certified))
-    print(dict_occupation)
-    print(dict_states)
 
-    return SAMPLES
+    return total_certified
 
 
-def feed_result(SAMPLES, filename):
+def calculate_percentage(number_certified, total_certified):
+    percentage = float(number_certified)/float(total_certified)
+    percentage *= 100
+    percentage = round(percentage, 1)
+    return percentage
+
+
+def feed_result_states(state_info, filename, total_certified):
     with open(filename, 'w') as f:
-        f.write("NO., CASE STATUS, OCCUPATION, STATE\n")
+        f.write("TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n")
         row_num = 1
-        for item in SAMPLES:
-            f.write("%s;" % row_num)
-            col_num = 0
-            for i in item:
-                if col_num != len(item)-1:
-                    f.write("%s;" % i)
-                else:
-                    f.write("%s" % i)
-                col_num += 1
-            f.write("\n")
+        for item in state_info:
+            if row_num <= 10:
+                percentage = calculate_percentage(item[1], total_certified)
+                f.write("%s;" % item[0])
+                f.write("%s;" % item[1])
+                f.write("%s" % percentage)
+                f.write("%")
+                f.write("\n")
             row_num += 1
 
 
-if __name__ == "__main__":
-    filename = '../insight_testsuite/tests/test_1/input/h1b_input.csv'
-    output_file = '../insight_testsuite/tests/test_1/output/result.txt'
-    SAMPLES = h1b_report_csv(filename)
-    feed_result(SAMPLES, output_file)
+def feed_result_occupation(occupation_info, filename, total_certified):
+    with open(filename, 'w') as f:
+        f.write("TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n")
+        row_num = 1
+        for item in occupation_info:
+            if row_num <= 10:
+                percentage = calculate_percentage(item[1], total_certified)
+                f.write("%s;" % item[0])
+                f.write("%s;" % item[1])
+                f.write("%s" % percentage)
+                f.write("%")
+                f.write("\n")
+            row_num += 1
 
+
+def sort_condition(dict):
+    sorted_dic = sorted(dict.items(), key=lambda x: (-x[1], x[0]))
+    return sorted_dic
+
+
+def h1b_report(input_filename,output_file_occupation,output_file_states):
+    dict_states = {}
+    dict_occupation = {}
+
+    total_certified = parsecsv(input_filename, dict_states, dict_occupation)
+
+    sorted_dict_states = sort_condition(dict_states)
+    sorted_dict_occupation = sort_condition(dict_occupation)
+
+    feed_result_states(sorted_dict_states, output_file_states, total_certified)
+    feed_result_occupation(sorted_dict_occupation, output_file_occupation, total_certified)
+
+    print(sorted_dict_states)
+    print(sorted_dict_occupation)
+    print(total_certified)
+
+
+if __name__ == "__main__":
+    input_filename = '../insight_testsuite/tests/test_1/input/h1b_input.csv'
+    output_file_states = '../insight_testsuite/tests/test_1/output/top_states.txt'
+    output_file_occupation = '../insight_testsuite/tests/test_1/output/top_occupation.txt'
+
+    h1b_report(input_filename,output_file_occupation,output_file_states)
